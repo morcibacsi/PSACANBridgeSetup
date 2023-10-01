c7731a3ceb4180c777d92bd1a1558722fc6e5dc4
morcibacsi <pinterpeti@gmail.com>
2023-10-01 20:40:57 +0200

    Udated npm packages

--- webpack.config.js
+++ webpack.config.js
@@ -3,136 +3,142 @@ const path = require('path');
 const fs = require('fs');
 
 const TerserPlugin = require('terser-webpack-plugin');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 const CompressionPlugin = require('compression-webpack-plugin');
 const ProgmemGenerator = require('./progmem-generator.js');
 
 module.exports = (env, args) => {
-	const isProduction = args && args['mode'] === 'production';
-	console.log('');
-	console.log(isProduction ? 'PRODUCTION BUILD' : 'DEVELOPMENT BUILD');
-	console.log('');
+    const isProduction = args && args['mode'] === 'production';
+    console.log('');
+    console.log(isProduction ? 'PRODUCTION BUILD' : 'DEVELOPMENT BUILD');
+    console.log('');
 
 
-	const config = {
-		entry: {
-			'scripts/main': path.resolve('./src/bootstrap.tsx'),
-		},
-		output: {
-			path: path.resolve('./dist'),
-		},
-		target: 'web',
-		devtool: isProduction ? false : 'source-map',
-		optimization: {
-			splitChunks: {
-				// always create vendor.js
-				cacheGroups: {
-					vendor: {
-						test: /[\\/]node_modules[\\/]/,
-						name: 'scripts/vendor',
-						chunks: 'initial',
-						enforce: true,
-					},
-				},
-			},
-		},
-		resolve: {
-			extensions: ['.ts', '.tsx', '.js', '.html', '.txt'],
-		},
-		module: {
-			rules: [
-				{
-					test: /\.(ts|tsx)$/,
-					// eslint
-					enforce: 'pre',
-					use: [
-						{
-							options: {
-								eslintPath: require.resolve('eslint'),
-							},
-							loader: require.resolve('eslint-loader'),
-						},
-					],
-					exclude: /node_modules/,
-				},
-				{
-					test: /\.tsx?$/,
-					exclude: /node_modules/,
-					use: [{
-						loader: 'ts-loader',
-						options: {
-							transpileOnly: true,
-						},
-					}],
-				},
-				// app main .less file
-				{
-					test: /app\.less$/i,
-					use: [
-						{
-							loader: 'file-loader',
-							options: {
-								name: 'styles/[name].css',
-							}
+    const config = {
+        entry: {
+            'scripts/main': path.resolve('./src/bootstrap.tsx'),
+        },
+        output: {
+            path: path.resolve('./dist'),
+        },
+        target: 'web',
+        devtool: isProduction ? false : 'source-map',
+        optimization: {
+            splitChunks: {
+                // always create vendor.js
+                cacheGroups: {
+                    vendor: {
+                        test: /[\\/]node_modules[\\/]/,
+                        name: 'scripts/vendor',
+                        chunks: 'initial',
+                        enforce: true,
+                    },
+                },
+            },
+        },
+        resolve: {
+            extensions: ['.ts', '.tsx', '.js', '.html', '.txt'],
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.(ts|tsx)$/,
+                    // eslint
+                    enforce: 'pre',
+                    use: [
+                        {
+                            options: {
+                                eslintPath: require.resolve('eslint'),
+                            },
+                            loader: require.resolve('eslint-loader'),
+                        },
+                    ],
+                    exclude: /node_modules/,
+                },
+                {
+                    test: /\.tsx?$/,
+                    exclude: /node_modules/,
+                    use: [{
+                        loader: 'ts-loader',
+                        options: {
+                            transpileOnly: true,
+                        },
+                    }],
+                },
+                // app main .less file
+                {
+                    test: /app\.less$/i,
+                    use: [
+                        {
+                            loader: 'file-loader',
+                            options: {
+                                name: 'styles/[name].css',
+                            }
 
-						},
-						{
-							loader: 'less-loader',
-						}
-					]
-				},
+                        },
+                        {
+                            loader: 'less-loader',
+                        }
+                    ]
+                },
                 {
                     test: /\.css$/i,
-                    use: ["style-loader", "css-loader"],
+                    use: [
+                        MiniCssExtractPlugin.loader, 
+                        { loader: "style-loader" },
+                        { loader: "css-loader" },
+                        ],
                 },
                 {
                     test: /\.svg$/,
                     use: ['preact-svg-loader'],
                 }
-			],
-		},
+            ],
+        },
 
-		watchOptions: {
-			aggregateTimeout: 100,
-			ignored: /node_modules/,
-			poll: 300
-		},
+        watchOptions: {
+            aggregateTimeout: 100,
+            ignored: /node_modules/,
+            poll: 300
+        },
 
-		devServer: {
-			headers: {
-				'Access-Control-Allow-Origin': '*'
-			},
-			contentBase: './dist',
-			publicPath: '/',
-			compress: false,
-			port: 3030,
-			historyApiFallback: true,
-			hot: true,
-			inline: true,
-			stats: 'normal',
-			clientLogLevel: 'error'
-		},
+        devServer: {
+            headers: {
+                'Access-Control-Allow-Origin': '*'
+            },
+            contentBase: './dist',
+            publicPath: '/',
+            compress: false,
+            port: 3030,
+            historyApiFallback: true,
+            hot: true,
+            inline: true,
+            stats: 'normal',
+            clientLogLevel: 'error'
+        },
 
-		plugins: [
-			new webpack.EnvironmentPlugin({
-				NODE_ENV: isProduction ? 'production' : 'development',
-				DEBUG: !isProduction
-			}),
+        plugins: [
+            new webpack.EnvironmentPlugin({
+                NODE_ENV: isProduction ? 'production' : 'development',
+                DEBUG: !isProduction
+            }),
 
-			new CopyWebpackPlugin({
-				patterns: [
-					// static files to the site root folder (index and robots)
-					{
-						from: '**/*',
-						to: path.resolve('./dist/'),
-						context: './src/static/'
-					},
-				]
-			}),
+            new CopyWebpackPlugin({
+                patterns: [
+                    // static files to the site root folder (index and robots)
+                    {
+                        from: '**/*',
+                        to: path.resolve('./dist/'),
+                        context: './src/static/'
+                    },
+                ]
+            }),
             ///*
             new ProgmemGenerator({ outputPath: "WWWData.h", bytesPerLine: 20 }),
+            new MiniCssExtractPlugin(),
             /*
 
             new CompressionPlugin({
@@ -143,16 +149,16 @@ module.exports = (env, args) => {
             }),
             
             //*/
-		],
-	};
+        ],
+    };
 
-	if (isProduction) {
-		config.optimization.minimize = true;
-		config.optimization.minimizer = [
-			new TerserPlugin({extractComments: false}),
-			new OptimizeCSSAssetsPlugin({}),
-		]
-	}
+    if (isProduction) {
+        config.optimization.minimize = true;
+        config.optimization.minimizer = [
+            new TerserPlugin({extractComments: false}),
+            new CssMinimizerPlugin(),
+        ]
+    }
 
-	return config;
+    return config;
 };
